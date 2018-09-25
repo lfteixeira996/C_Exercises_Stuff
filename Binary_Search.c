5feba9414db79a7af448b146fd30f8c175a6ebc8
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-09-25 10:13:01 +0100

    Update Binary_Search.c

--- Binary_Search.c
+++ Binary_Search.c
@@ -1,31 +1,35 @@
 
 /*
-Linear search, i.e
+Binary search, i.e
 
-Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
-If x matches with an element, return the index.
-If x doesn’t match with any of elements, return -1.
+We basically ignore half of the elements just after one comparison.
+
+Compare x with the middle element.
+If x matches with middle element, we return the mid index.
+Else If x is greater than the mid element, then x can only lie in right half subarray after the mid element. So we recur for right half.
+Else (x is smaller) recur for the left half.
 
 */
 
 #include <stdio.h>
 
-int arr[] = {10, 20, 80, 30, 60};
-int binary_search(int search, int low, int high);
+int arr[] = {10, 20, 30, 60, 80};
+int binarySearch(int l, int r, int x);
 
 
 int main()
 {
-    int search, index;
+    int search, res, last_index;
     
     printf("Value to search: ");
     scanf("%d", &search);
     
-    index = binary_search(search, 0, sizeof(arr)/sizeof(int));
+    last_index = sizeof(arr)/sizeof(int)-1;
+    res = binarySearch(0, last_index, search);
     
-    if(index != -1)
+    if(res != -1)
     {
-        printf("Element %d is present at index %d\n", search, index);
+        printf("Element %d is present at index %d\n", search, res);
     }
     
     else
@@ -36,24 +40,28 @@ int main()
     return 0;
 }
 
-int binary_search(int s, int l, int h)
-{
-    int m_idx = l + (h-l)/2;
-    
-    if(s == arr[m_idx])
-    {
-        return m_idx;
-    }
-    
-    else if(s > arr[m_idx])
-    {
-        return binary_search(s, m_idx, h);
-    }
-
-    else if(s < arr[m_idx])
-    {
-        return binary_search(s, l, m_idx);
-    }
-
-    return -1;
-}
+int binarySearch(int l, int h, int x) 
+{ 
+   if (h >= l) 
+   { 
+        int mid = l + (h - l)/2; 
+  
+        // If the element is present at the middle  
+        // itself 
+        if (arr[mid] == x)   
+            return mid; 
+  
+        // If element is smaller than mid, then  
+        // it can only be present in left subarray 
+        if (arr[mid] > x)  
+            return binarySearch( l, mid-1, x); 
+  
+        // Else the element can only be present 
+        // in right subarray 
+        return binarySearch( mid+1, h, x); 
+   } 
+  
+   // We reach here when element is not  
+   // present in array 
+   return -1; 
+} 
