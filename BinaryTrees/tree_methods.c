f3a32215143ebddcd913d195f9056577eb34ce8c
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-09-28 15:02:56 +0100

    Update tree_methods.c

--- BinaryTrees/tree_methods.c
+++ BinaryTrees/tree_methods.c
@@ -2,7 +2,26 @@
 #include "tree_methods.h"
 
 
+/*  OPTIONS */
+//Print
+void print_option(tnode* root)
+{
+    printf("\n");
+    PrintTree(root);
+    printf("\n");
+}
 
+//Free
+tnode *free_option(tnode* root)
+{
+    root = freeAlTree(root);
+    printf("\nTree Deleted!\n"); 
+    return root;
+}
+
+
+
+/***************************************/
 
 /*Create a new Node*/
 tnode *CreateNode(int val)
@@ -17,8 +36,8 @@ tnode *CreateNode(int val)
 }
 
 
-/*Create a new Tree*/
-tnode *CreateTree(tnode* root, int val)
+/*Insert a node in the Tree*/
+tnode *insertNode(tnode* root, int val)
 {   
     /*
         side = 0 - right node
@@ -78,6 +97,72 @@ tnode *CreateTree(tnode* root, int val)
 }
 
 
+/*remove a node from the Tree*/
+tnode *deleteNode(tnode* root, int val)
+{   
+    /*
+        side = 0  - right node
+        side = 1  - left node
+        side = -1 - rep--
+    */
+    int side;
+    
+    tnode *next_node = root;
+    tnode *curr_node = root;
+    tnode *prev_node = root;
+    
+    /*If Tree is Empty*/
+    if(root == NULL)
+    {
+        printf("Tree is Empty!");
+        return root;
+    }
+    
+    
+    /*remove the root*/
+    else if(root->data == val)
+    {
+        /*check if the rep of root is bigger than 1*/
+        if(root->rep > 1)
+        {
+            root->rep--;
+        }
+        
+        /*if rep is 1, remove all tree (all nodes)*/
+        else
+        {
+            freeAlTree(root);
+        }
+        
+        return root;
+    }
+    
+    
+    
+    
+    return root;
+}
+
+
+/*Free all nodes of the Tree*/
+tnode *freeAlTree(tnode* root)
+{
+    if (root == NULL)  return;
+    
+    /* first delete both subtrees */
+    freeAlTree(root->left); 
+    freeAlTree(root->right); 
+    
+    /* then delete the node */
+    printf("\nDeleting node: %d", root->data); 
+    free(root); 
+    root = NULL;
+    
+ 
+    return root;
+}
+
+
 /*Print the tree*/
 void PrintTree(tnode* root)
 {
@@ -99,5 +184,4 @@ void PrintTree(tnode* root)
     printf("\n");
     PrintTree(root->left);
     PrintTree(root->right);
-    
 }
