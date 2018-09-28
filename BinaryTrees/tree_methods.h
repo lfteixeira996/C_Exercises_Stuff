b46e31b6cd31d8ee80d822570427abdd7e7be8e6
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-09-28 11:03:31 +0100

    Update tree_methods.h

--- BinaryTrees/tree_methods.h
+++ BinaryTrees/tree_methods.h
@@ -2,8 +2,17 @@
 #define METHODS_FILE
 
 
-   
+typedef struct node
+{
+    int data;
+    int rep;
+    struct node *left;
+    struct node *right;
+}tnode;
 
 
+tnode *CreateNode(int val);  
+tnode *CreateTree(tnode* root, int val);
+void *PrintTree(tnode* root);
 
 #endif
