a09bae84eb193da3e87e954dffa88574c04db4fc
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-09-28 15:02:38 +0100

    Update main.c

--- BinaryTrees/main.c
+++ BinaryTrees/main.c
@@ -2,17 +2,26 @@
 #include "tree_methods.h"
 
 
+
+
 int main()
 {
     int i;
-    int array[5] = {1, 3, 4, 4, 4};
+    int array[3] = {1, 3, 4};
     tnode *root = NULL;
     
     for (i=0; i<sizeof(array)/sizeof(int); i++) {
-        root = CreateTree(root, array[i]);
+        root = insertNode(root, array[i]);
     }
     
-    PrintTree(root);
+    print_option(root);
+    root = free_option(root);
+
+    for (i=0; i<sizeof(array)/sizeof(int); i++) {
+        root = insertNode(root, 1);
+    }
+
+    print_option(root);
     
     return 0;
 }
