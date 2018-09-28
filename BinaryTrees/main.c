9b9351ad02ec9274cfbec7f476245900c1749d04
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-09-28 10:16:25 +0100

    Create main.c

---
+++ BinaryTrees/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int array = {8, 4, 3};
+    
+    
+    for (i=0; i<sizeof(array)/sizeof(int); i++) {
+       
+    }
+    
+    
+    
+    return 0;
+}
