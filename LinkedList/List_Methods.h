8ae1f62beca38f8f56037c3508bf71a94205584e
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-10-02 10:29:50 +0100

    Update List_Methods.h

--- LinkedList/List_Methods.h
+++ LinkedList/List_Methods.h
@@ -12,5 +12,6 @@ node_t* create_node(int data);
 node_t* push(node_t *root, int data);
 node_t* pop(node_t *root);
 void print_node(node_t *node_to_print);
+void search_node(node_t *root, int data);
 
 #endif
